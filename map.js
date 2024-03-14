75d533acc97dc08f0ffd5336d29885d526e26a06
alina <a.sarzhanova@g.nsu.ru>
2024-03-14 15:21:21 +0100

    First Commit

---
+++ map.js
@@ -0,0 +1,21 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+let loader = new THREE.ObjectLoader();
+loader.load('model.json', function(mesh) {
+    scene.add(mesh);
+});
+
+function animate() {
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+}
+
+animate();
