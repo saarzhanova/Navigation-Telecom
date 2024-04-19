6d116410611b75f9bfd3a08afa7f2cbcd0bdeb6c
alina <a.sarzhanova@g.nsu.ru>
2024-04-19 22:02:38 +0100

    refactor background color

--- main.js
+++ main.js
@@ -1,42 +1,30 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import {OBJLoader} from 'three/addons/loaders/OBJLoader.js';
+import {Color} from "three";
 
 const scene = new THREE.Scene();
+scene.background = new Color('grey');
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const aspect = screen.width / screen.height;
-
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-camera.position.set(0, 0, 5); // Position the camera
-camera.lookAt(scene.position); //add this line
+camera.position.set(0, 0, 5);
 
-// Add ambient light to the scene
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-scene.add(ambientLight);
+const gltfloader = new GLTFLoader();
+const urlGLB = 'glb/Telecom.glb';
 
-// Add directional light to the scene
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-directionalLight.position.set(0, 1, 0); // Set light position
-scene.add(directionalLight);
+gltfloader.load( urlGLB, function ( gltf ) {
+    let object = gltf.scene
+    scene.add( object);
 
-const loader = new GLTFLoader();
-const url = 'Telecom.glb';
-loader.load(url, (gltf) => {
-    const root = gltf.scene;
+}, undefined, function ( error ) {
+    console.error( error );
+} );
 
-    // Adjust the scale of the model
-    root.scale.set(0.1, 0.1, 0.1);
-
-    // Center the model
-    const box = new THREE.Box3().setFromObject(root);
-    const center = box.getCenter(new THREE.Vector3());
-    root.position.sub(center);
-
-    scene.add(root);
-});
+camera.lookAt(scene.position);
 
 function animate() {
     requestAnimationFrame(animate);
